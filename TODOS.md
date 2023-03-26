2df9b4dc916c99b0b55d9855f0118e6414c4a248
Roger Santos <roger.santos36@gmail.com>
2023-03-26 16:49:04 -0300

    :sparkles: feat: adicionado display de result e button submit

--- TODOS.md
+++ TODOS.md
@@ -1,1 +1,1 @@
-- [ ] - todo conteúdo dento de um form
+- [ ] - 
--- app/js/app.js
+++ app/js/app.js
@@ -3,10 +3,47 @@ const currencyTwoContainer = document.querySelector('[data-js="currency-two"]')
 const currencyOneOptionsContainer = document.querySelector('[data-js="currency-one-options-container"]')
 const currencyTwoOptionsContainer = document.querySelector('[data-js="currency-two-options-container"]')
 
-currencyOneContainer.addEventListener('click', () => {
-    currencyOneOptionsContainer.classList.toggle('show-options')
+const closeCurrencyContainer = () => {
+    currencyOneOptionsContainer.classList.remove('show-options')
+    currencyTwoOptionsContainer.classList.remove('show-options')
+}
+
+const handleCurrencyContainerVisibility = event => {
+    event.stopPropagation()
+
+    const clickedCurrencyContainer = event.currentTarget.dataset.js
+    const isClickedCurrencyOne = clickedCurrencyContainer === 'currency-one'
+    
+    if (isClickedCurrencyOne) {
+        currencyOneOptionsContainer.classList.toggle('show-options')
+        currencyTwoOptionsContainer.classList.remove('show-options')
+        return
+    }
+
+    currencyTwoOptionsContainer.classList.toggle('show-options')
+    currencyOneOptionsContainer.classList.remove('show-options')
+}
+
+currencyOneOptionsContainer.addEventListener('click', event => {
+    event.stopPropagation()
+
+    const selectedCurrency = event.target.closest('.currency__content-item')
+    
+    if (selectedCurrency) {
+        currencyOneOptionsContainer.classList.remove('show-options')
+    }
 })
 
-currencyTwoContainer.addEventListener('click', () => {
-    currencyTwoOptionsContainer.classList.toggle('show-options')
+currencyTwoOptionsContainer.addEventListener('click', event => {
+    event.stopPropagation()
+
+    const selectedCurrency = event.target.closest('.currency__content-item')
+    
+    if (selectedCurrency) {
+        currencyTwoOptionsContainer.classList.remove('show-options')
+    }
 })
+
+document.addEventListener('click', closeCurrencyContainer)
+currencyOneContainer.addEventListener('click', handleCurrencyContainerVisibility)
+currencyTwoContainer.addEventListener('click', handleCurrencyContainerVisibility)
