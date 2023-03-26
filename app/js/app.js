6042c784ff0232676d9fef48fe8314c0f4221893
Roger Santos <roger.santos36@gmail.com>
2023-03-26 20:09:36 -0300

    :sparkles: feat: add currencies into first select

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,41 @@ const currencyTwoContainer = document.querySelector('[data-js="currency-two"]')
 const currencyOneOptionsContainer = document.querySelector('[data-js="currency-one-options-container"]')
 const currencyTwoOptionsContainer = document.querySelector('[data-js="currency-two-options-container"]')
 
+const generateOptions = async () => {
+    const { conversion_rates } = await getExchangeRate()
+    const countries = await getCountries()
+
+    const options = Object.keys(conversion_rates).map(currency => {
+        const flagCode = currency.slice(0, 2).toLowerCase()
+        const { code, countryName } = countries.find(country => country.code === currency)
+
+        return `<li class="currency__content-item">
+            <div class="currency__content-image">
+                <img 
+                    src="https://flagcdn.com/w80/${flagCode}.png" 
+                    srcset="https://flagcdn.com/w160/${flagCode}.png 2x"
+                    width="50"
+                />
+            </div>
+
+            <div class="currency-name">
+                <h2>${currency}</h2>
+                <span>${countryName}</span>
+            </div>
+
+            <div class="arrow-right">
+                <i class="ri-arrow-right-s-line"></i>
+            </div>
+        </li>`
+    })
+
+    const formattedOptions = options.join().replaceAll(',', '')
+
+    currencyOneOptionsContainer.innerHTML = formattedOptions
+}
+
+generateOptions()
+
 const closeCurrencyContainer = () => {
     currencyOneOptionsContainer.classList.remove('show-options')
     currencyTwoOptionsContainer.classList.remove('show-options')
---
+++ app/js/exchange.js
@@ -0,0 +1,31 @@
+const APIKey = '6a00e715b1886dc966bf1b0d'
+
+const getExchangeRate = async () => {
+  try {
+    const request = await fetch(`https://v6.exchangerate-api.com/v6/${APIKey}/latest/USD`)
+    const exchangeRate = await request.json()
+
+    if (!request.ok) {
+      throw new Error ('Não foi possível encontrar a moeda.')
+    }
+    return exchangeRate
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getCountries = async () => {
+  try {
+    const request = await fetch('./app/json/currencies.json')
+    const exchangeInfo = request.json()
+
+    if (!request.ok) {
+      throw new Error ('Não foi possível obter as informações da moeda.')
+    }
+
+    return exchangeInfo
+  } catch ({error, message}) {
+    console.log(`${error}: ${message}`)
+  }
+}
