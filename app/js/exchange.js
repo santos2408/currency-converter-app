72b5923ac9593cd853512b22a15aa7a7f4f97feb
Roger Santos <roger.santos36@gmail.com>
2023-04-10 19:22:49 -0300

    :sparkles: feat: add feedback error on api request

--- app/js/exchange.js
+++ app/js/exchange.js
@@ -1,12 +1,28 @@
 const APIKey = '6a00e715b1886dc966bf1b0d'
 
+const getUrl = selectedCurrency => `https://v6.exchangerate-api.com/v6/${APIKey}/latest/${selectedCurrency}`
+
+const getErrorMessage = errorType => ({
+  'unsupported-code': 'A moeda não existe em nosso banco de dados.',
+  'malformed-request': 'O endpoint do seu request precisa seguir a estrutura a seguir https://v6.exchangerate-api.com/v6/API/latest/USD',
+  'invalid-key': 'A chave da API não é válida.',
+  'inactive-account': 'Seu endereço de email não foi confirmado.',
+  'quota-reached': 'Sua cota alcançou o limite de requests permitido no seu plano atual.'
+})[errorType] || 'Não foi possível encontrar a moeda.'
+
 const fetchExchangeRate = async selectedCurrency => {
   try {
-    const request = await fetch(`https://v6.exchangerate-api.com/v6/${APIKey}/latest/${selectedCurrency}`)
-    const exchangeRateData = await request.json()
+    const url = getUrl(selectedCurrency)
+    const response = await fetch(url)
+    const exchangeRateData = await response.json()
 
-    if (!request.ok) {
-      throw new Error ('Não foi possível encontrar a moeda.')
+    if (exchangeRateData.result === 'error') {
+      const errorMessage = getErrorMessage(exchangeRateData['error-type'])
+      throw new Error (errorMessage)
+    }
+
+    if (!response.ok) {
+      throw new Error ('Sua conexão falhou. Não foi possível obter as informações.')
     }
 
     return state.setExchangeRate(exchangeRateData)
